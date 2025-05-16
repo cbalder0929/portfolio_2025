4762f03c12594d039ee2914c5adf39a1d90c2da7
cbalder0929 <balderas02@colum.edu>
2025-05-16 00:29:01 -0500

    Add files via upload

---
+++ Finale/script.js
@@ -0,0 +1,32 @@
+// Working Navigation Toggle
+document.querySelector('.navToggle').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('hidden');
+});
+
+// Corrected Carousel Code
+const carouselButtons = document.querySelectorAll("[data-carousel-button]");
+
+carouselButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+        const activeSlide = slides.querySelector("[data-active]");
+        const slidesArray = [...slides.children];
+        let newIndex = slidesArray.indexOf(activeSlide) + offset;
+
+        if (newIndex < 0) newIndex = slidesArray.length - 1;
+        if (newIndex >= slidesArray.length) newIndex = 0;
+
+        slidesArray[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    });
+});
+	
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
